4867ad57ca5360b10f1b1bf05d18761ae341946e
PetarVujetic <97petar@live.com>
2021-11-17 01:47:42 +0100

    OK

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const UAParser = require('ua-parser-js');
 var parser = new UAParser();
 
 
-app.use('/', (req, res) => {
+app.use('/getDevice', (req, res) => {
     var ua = req.headers['user-agent'];     // user-agent header from an HTTP request
     console.log(parser.setUA(ua).getResult());
-    res.json({ msg: parser.setUA(ua).getResult()});
+    res.status(200).json({ msg: parser.setUA(ua).getResult()});
 })
 
 
